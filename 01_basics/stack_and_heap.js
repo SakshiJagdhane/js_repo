05c5257f8351f2d4c7dd217233a560e58a7c458c
SakshiJagdhane <133689114+SakshiJagdhane@users.noreply.github.com>
2025-03-19 18:18:27 +0000

    change

---
+++ 01_basics/stack_and_heap.js
@@ -0,0 +1,4 @@
+//non-primitive datatype jatat heap madhe
+//primtive vale jatat stack madhe
+//stack madhe reference madhe change keyavar original nahi change hot
+//heap madhe donhi pan chnage houn jate
