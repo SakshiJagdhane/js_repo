7ce82b0a42f60ea2acfd6d772b47ee8339ca4cda
SakshiJagdhane <133689114+SakshiJagdhane@users.noreply.github.com>
2025-03-19 17:56:34 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,36 @@
+let value=3
+let negvalue=-value
+console.log(negvalue); 
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+//boolean madhe "" mhanje false;
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+
+num1=num2=num3=2*2
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+console.log("2" >1);
+console.log("02" >1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+const heroes=["shaktiman","durga"];
+
+let myObj={
+    name:"hitesh",
+    age:15,
+}
+
+const myfunction=function(){
+    console.log("Hi");
+}
